9e5818666ac91ae750931ec8720a378959a69f68
vlada <vladavr3@gmail.com>
2023-02-13 21:46:46 +0100

    initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,54 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        for (int i = 0; i < 10; i++){
+//            System.out.println(i);
+//            if (i == 9){
+//                System.out.println("Nemate dovoljno sredstava!");
+//            }
+//        }
+
+        for (int i = 0; i < 5;i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("Iterator: " + i + " brojac j: " + j);
+            }
+        }
+
+
+
+//        for (;;) {
+//            System.out.println("Infinite");
+//        }
+
+//        int a = 0;
+//        while (a<10){
+//            a++;
+//            if (a == 5){
+//                continue;
+//            }else {
+//                System.out.println(a);
+//            }
+//        }
+
+//        while (true){
+//            System.out.println("Infinite");
+//            break;
+//        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
